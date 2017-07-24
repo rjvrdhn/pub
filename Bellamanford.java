107ccd15d8d9589203cc19ace00bd3ccc284422d
rjvrdhn <rajavardhan@outlook.com>
2017-07-24 14:23:38 -0500

    Create Bellamanford.java

---
+++ Bellamanford.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+// one class needs to have a main() method
+public class HelloWorld
+{
+  // arguments are passed using the text field below this editor
+  public static void main(String[] args)
+  {
+    Graph myGraph = new Graph(5, 5);
+   // System.out.print(myGraph);
+  }
+}
+
+public class Edge
+{
+  private int source, destination, distance;
+  
+  public Edge() 
+  {
+    source =0;
+    destination=0;
+    distance=0;
+  }
+  
+  public void SetSource(int src)
+  {
+    source = src;
+  }
+  public int GetSource()
+  {
+    return source;
+  }
+  public void SetDestination(int dest)
+  {
+    destination = dest;
+  }
+  public int GetDestination()
+  {
+    return destination;
+  }
+  public void SetDistance(int dist)
+  {
+    distance = dist;
+  }
+  public int GetDistance()
+  {
+    return distance;
+  }
+}
+
+
+public class Graph
+{
+  private ArrayList<Edge> edges;
+  private int V;
+  private int E;
+  public Graph(int nOfV, int nOfE)
+  {
+    V = nOfV;
+    E = nOfE;
+    edges = new ArrayList<Edge>(nOfE);
+  }
+  public int GetNumberOfVertices()
+  {
+    return V;
+  }
+  public ArrayList<Edge> GetEdges()
+  {
+    return edges;
+  }
+  public void SetEdge(int index, Edge newEdge)
+  {
+    edges.set(index, newEdge);
+  }
+}
+
+public class Bellmanford
+{
+  
+}
