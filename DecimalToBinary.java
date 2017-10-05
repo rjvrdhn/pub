c70dfa8edd653b71997f418db3d001e3e4addb2d
rjvrdhn <rajavardhan@outlook.com>
2017-10-05 09:29:12 -0500

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
+
 public class DecimalToBinary 
 {
 	
-	public String decimalToBinary(double n)
+	public String decimalToBinary(String in)
 	{
-		double num = 34.56;
 		String bin = "";
+		StringBuilder sb = new StringBuilder();
+		try
+		{
+			int num = Integer.parseInt(in);
+			while(num>0)
+			{
+				bin = bin + num%2;
+				num = num/2;
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.print("error\n");
+		}
 		
-		
-		
+		if(!bin.isEmpty())
+		{
+			sb.append(bin);
+			sb.reverse();
+			bin = sb.toString();
+		}
 		return bin;
 	}
 
@@ -16,7 +35,20 @@ public class DecimalToBinary
 	 */
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		
+		String in;
+		String bin;
+		Scanner sc = new Scanner(System.in);
+		DecimalToBinary db = new DecimalToBinary();
+		while(true)
+		{
+			System.out.print("Please enter a number or e to exit: ");
+			in = sc.next();
+			if(in.equals("e"))
+				break;
+			bin = db.decimalToBinary(in);
+			System.out.print("Result: "+bin+"\n");
+		}
 
 	}
 
