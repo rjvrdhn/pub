a5d578a9ccd8afe8ebd3e4359e10fcdb25662e55
rjvrdhn <rajavardhan@outlook.com>
2017-10-05 07:13:32 -0500

    Create DecimalToBinary.java

---
+++ DecimalToBinary.java
@@ -0,0 +1,23 @@
+public class DecimalToBinary 
+{
+	
+	public String decimalToBinary(double n)
+	{
+		double num = 34.56;
+		String bin = "";
+		
+		
+		
+		return bin;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
